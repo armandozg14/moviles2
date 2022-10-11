30db7894629d0b5389ee952c753a5de01e1b87c0
analourdescr <ana_lourdes98@hotmail.com>
2022-10-11 07:12:53 -0700

    Initial Commit

---
+++ moviles2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  moviles2
+//
+//  Created by Alumno on 10/11/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
